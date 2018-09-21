7720260a1bbb6f5dbfd93fc3e1268ad8adabd789
beergeek <brett@puppet.com>
2018-09-21 17:25:27 +1000

    Best practices and style, plus R&P format

--- site/profile/manifests/database.pp
+++ site/profile/manifests/database.pp
@@ -1,3 +1,18 @@
-class profile::database {
-  include databases
+class profile::database (
+  Hash              $dbs,
+  Sensitive[String] $mysql_password,
+) {
+
+  class { '::mysql::server':
+    root_password           => $mysql_password,
+    remove_default_accounts => true,
+  }
+
+  $dbs.each |String $db_name, Hash $db_data| {
+    mysql::db { $db_name:
+      user     => "${db_name}-user",
+      password => Sensitive.new($db_data['password']),
+      *        => delete($db_data,'password'),
+    }
+  }
 }
--- site/profile/manifests/website.pp
+++ site/profile/manifests/website.pp
@@ -1,3 +1,41 @@
-class profile::website {
-  include websites
+class profile::website (
+  Hash $vhosts,
+) {
+  $docroot = '/var/www/vhost'
+
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  group { 'www-data':
+    ensure => present,
+  }
+
+  user { 'www-data':
+    ensure => present,
+    shell  => '/bin/true',
+    gid    => 'www-data',
+  }
+
+  file { $docroot:
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+  }
+
+  $vhosts.each |Integer $index, String $vhost| {
+    apache::vhost { $vhost:
+      port          => 8000 + $index,
+      docroot       => "${docroot}/${vhost}",
+      docroot_owner => 'www-data',
+      docroot_group => 'www-data',
+    }
+
+    file { "${docroot}/${vhost}/index.html":
+      ensure => present,
+      owner => 'www-data',
+      group => 'www-data',
+      content => "<HTML><HEAD><TITLE>Test website ${vhost}</TITLE></HEAD><BODY>Just a test website for vhost ${vhost}.</BODY></HTML>",
+    }
+  }
 }
